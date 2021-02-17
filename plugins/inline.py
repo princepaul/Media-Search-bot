c4193e22e9a432442f7a035d273912a56ba1f0e2
Prince Paul <princepaul@users.noreply.github.com>
2021-02-17 15:50:39 +0530

    Adding Caption to Description

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,7 +31,7 @@ async def answer(bot, query):
                 title=file.file_name,
                 file_id=file.file_id,
                 caption=file.caption or "",
-                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
+                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}\nDescription: {file.caption}',
                 reply_markup=reply_markup))
 
     if results:
